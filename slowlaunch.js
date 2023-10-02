6392bc858b473b4670573b50feb2d0d97f87812d
calebtkim <33525944+calebtkim@users.noreply.github.com>
2023-10-02 11:27:21 -0700

    Update slowlaunch.js to send header

--- slowlaunch.js
+++ slowlaunch.js
@@ -10,6 +10,7 @@ const server = http.createServer((request, response) => {
  }
 
  response.writeHead(200, { "Content-Type": "text/plain" });
+ response.setHeader("application-startup-duration", 1);
  response.end("Hello 3002 :) !!");
 });
 
