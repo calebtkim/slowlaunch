6ed8f5cc5476e8385a2987b5112d49de23ef1439
calebtkim <33525944+calebtkim@users.noreply.github.com>
2023-10-20 11:02:22 -0700

    Update slowlaunch.js

--- slowlaunch.js
+++ slowlaunch.js
@@ -23,7 +23,7 @@ function sleep(ms) {
 }
 
 var start = Date.now();
-var end = start + 290000; // 3 minutes
+var end = start + 295000; // 3 minutes
 for (var i = 0; ; i++) {
   console.log(Date.now() + " " + i + " "+ factorial(i));
   sleep(2000)
